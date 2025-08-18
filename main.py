35b2a7d336936c1e21730061e9c8d6d20f9d64c1
felipe alencar <felipealencardev@gmail.com>
2025-08-18 13:46:44 -0300

    adding groups

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    updatable.add(player)
+    drawable.add(player)
 
     while True:
         for event in pygame.event.get():
@@ -21,8 +25,10 @@ def main():
     
         pygame.Surface.fill(screen, BACKGROUND_COLOR)
 
-        player.draw(screen)
-        player.update(dt)
+        for upd in updatable:
+            upd.update(dt)
+        for drw in drawable:
+            drw.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
